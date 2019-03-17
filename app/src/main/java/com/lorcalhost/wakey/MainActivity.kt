896124bbfef56dc1f114e148eb9bc925922b496b
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2019-03-17 14:42:25 +0100

    NFC recognition completely working, added more face resources.

--- app/src/main/java/com/lorcalhost/wakey/MainActivity.kt
+++ app/src/main/java/com/lorcalhost/wakey/MainActivity.kt
@@ -1,63 +1,53 @@
 package com.lorcalhost.wakey
 
+import android.app.PendingIntent
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.provider.Settings
-import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat
-import android.view.View
-import android.view.WindowManager
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.NfcManager
-import java.io.IOException
-import android.graphics.Color
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var sensor: Sensor? = null
     private var sensorManager: SensorManager? = null
+    private var isFaceWhite = false
 
+    private var nfcAdapter : NfcAdapter? = null
+    private var nfcPendingIntent: PendingIntent? = null
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-
-    }
-
-    private var isRunning = false
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        try{
-            if (event!!.values[0] < 8 && isRunning == false){
-                isRunning = false
-                myBackground.setBackgroundColor(Color.rgb(0,0,0))
-                NFCsupport.setTextColor(Color.rgb(255, 255, 255))
-                NFCstatus.setTextColor(Color.rgb(255, 255, 255))
-            }
-            else {
-                isRunning = false
-                myBackground.setBackgroundColor(Color.rgb(200,224,236))
-                NFCsupport.setTextColor(Color.rgb(0, 0, 0))
-                NFCstatus.setTextColor(Color.rgb(0, 0, 0))
-            }
-        }
-        catch (e : IOException){
-
-        }
-    }
-
-    // NFC adapter for checking NFC state in the device
-    private val nfcAdapter : NfcAdapter? = null
+    private var currentWhiteFace = R.drawable.facecute2w
+    private var currentBlackFace = R.drawable.facecute2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkNFCStatus()
+
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        nfcPendingIntent = PendingIntent.getActivity(this, 0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+
+        if (intent != null) {
+            processIntent(intent)
+        }
+
+
         myBackground.setBackgroundColor(Color.rgb(200,224,236))
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         hideStatusBar()
@@ -65,47 +55,89 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
 
-        val actionBar = supportActionBar
-        actionBar?.hide()
-
         btnNFCsettings.setOnClickListener {
-            startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+            showNFCSettings()
             NFCstatus.visibility = View.INVISIBLE
             btnNFCsettings.visibility = View.INVISIBLE
 
         }
-        checkNFCStatus()
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.values[0] < 10){
+            isFaceWhite = true
+            myBackground.setBackgroundColor(Color.rgb(0,0,0))
+            NFCstatus.setTextColor(Color.rgb(255, 255, 255))
+            cuteFace.setImageResource(currentWhiteFace)
+        }
+        else {
+            isFaceWhite = false
+            myBackground.setBackgroundColor(Color.rgb(200,224,236))
+            NFCstatus.setTextColor(Color.rgb(0, 0, 0))
+            cuteFace.setImageResource(currentBlackFace)
+        }
 
 
     }
 
     override fun onResume() {
         super.onResume()
+        nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
         sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
     }
 
     override fun onPause() {
         super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
         sensorManager!!.unregisterListener(this)
     }
 
-    override fun onNewIntent(intent: Intent){
-        setIntent(intent)
-        Toast.makeText(this, intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES).toString(), Toast.LENGTH_LONG).show()
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) processIntent(intent)
+    }
 
-        if (true){
-            cuteFace.setImageResource(R.drawable.kawaii)
+
+    private fun processIntent(checkIntent: Intent) {
+        if (checkIntent.action == NfcAdapter.ACTION_NDEF_DISCOVERED) {
+            val rawMessages = checkIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            if (rawMessages != null && rawMessages.isNotEmpty()) {
+                val ndefMsg = rawMessages[0] as NdefMessage
+                if (ndefMsg.records != null && ndefMsg.records.isNotEmpty()) {
+                    val ndefRecord = ndefMsg.records[0]
+                    if(ndefRecord.toUri().toString() == "bedroom"){
+                        Toast.makeText(this, "Goodnight!", Toast.LENGTH_SHORT).show()
+                        currentBlackFace = R.drawable.facesleep3
+                        currentWhiteFace = R.drawable.facesleep3w
+
+                        if(isFaceWhite){
+                            cuteFace.setImageResource(currentWhiteFace)
+                        } else {
+                            cuteFace.setImageResource(currentBlackFace)
+                        }
+
+                    }
+                    else {
+                        Toast.makeText(this, "NFC tag not recognized", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+
         }
     }
-    private fun hideStatusBar(){
-        window.setFlags(AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT, AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT)
-        window.decorView.systemUiVisibility = 3328
-    }
+
+
 
     private fun checkNFCStatus(){
-        val isNfcSupported: Boolean = this.nfcAdapter == null
-        if (!isNfcSupported) {
-            NFCsupport.text = "Nfc is not supported on this device"
+        val isNfcSupported: Boolean = nfcAdapter != null
+        if (isNfcSupported) {
             Toast.makeText(this, "Nfc is not supported on this device", Toast.LENGTH_SHORT).show()
             finish()
         } else {
@@ -117,4 +149,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
         }
     }
+
+    private fun showNFCSettings() {
+        Toast.makeText(this, "You need to enable NFC", Toast.LENGTH_SHORT).show()
+        val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun hideStatusBar(){
+        val actionBar = supportActionBar
+        actionBar?.hide()
+        window.setFlags(AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT, AccessibilityNodeInfoCompat.ACTION_NEXT_HTML_ELEMENT)
+        window.decorView.systemUiVisibility = 3328
+    }
+
 }
